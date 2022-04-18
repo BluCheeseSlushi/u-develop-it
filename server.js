ab43cadd1e8b438a634c7f24b0027debf6279632
Josiah Douglass <josiahdouglass64@gmail.com>
2022-04-18 07:04:20 -0400

    server and database connected

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require('express');
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello World'
-    });
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Ilovemitzi0!',
+        database: 'election'
+    },
+    console.log('Database connected')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
 });
 
 app.use((req,res) => {
